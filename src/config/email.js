91de7d37bb0d948efd8910c0c07de06d98df929e
Pooja4183 <poojatomar.jt@gmail.com>
2023-06-03 17:54:36 +0530

    Updated email notification with new fields

--- src/config/email.js
+++ src/config/email.js
@@ -13,18 +13,32 @@ const transporter = nodemailer.createTransport({
 const mailOptions = {
   from: process.env.FROM,
   to: process.env.TO,
-  subject: process.env.SUBJECT,
 };
 
 const sendEmail = (property) => {
-  let messageTemplate = process.env.TEXT;
+  const subjectTemplate = process.env.SUBJECT;
+  const subject = subjectTemplate
+    .replace("${property.contactName}", property.contactName);
+  mailOptions.subject = subject;
 
+  const messageTemplate = process.env.TEXT;
   const message = messageTemplate
-  .replace('${property.contactName}', property.contactName)
-  .replace('${property.contactPhone}', property.contactPhone)
-  .replace('${property.contactEmail}', property.contactEmail);
-
-
+    .replace("${property.contactName}", property.contactName)
+    .replace("${property.contactPhone}", property.contactPhone)
+    .replace("${property.contactEmail}", property.contactEmail)
+    .replace("${property.homeType}", property.homeType)
+    .replace("${property.isBuy}", property.isBuy)
+    .replace("${property.bed}", property.bed)
+    .replace("${property.bath}", property.bath)
+    .replace("${property.price}", property.price)
+    .replace("${property.currency}", property.currency)
+    .replace("${property.sqFt}", property.sqFt)
+    .replace("${property.address}", property.address)
+    .replace("${property.description}", property.description)
+    .replace("${property.yearBuilt}", property.yearBuilt)
+    .replace("${property.sellerType}", property.sellerType)
+    .replace("${property.isListed}", property.isListed)
+    .replace("${property.sellDuration}", property.sellDuration);
 
   mailOptions.html = message;
   console.log(message);
