13c42566651743538cfc786dda36fcb7967a1ea3
Pooja4183 <poojatomar.jt@gmail.com>
2023-06-10 19:37:09 +0530

    Impl. Query param status. Reduced code.

--- src/service/propertySearchService.js
+++ src/service/propertySearchService.js
@@ -1,5 +1,5 @@
 const propertySearchRouter = require("express").Router(),
-  {propertyDB, totalRecords} = require("../model/property");
+  { propertyDB, totalRecords } = require("../model/property");
 
 /**
  * Searches a property a property by id.
@@ -8,28 +8,43 @@ propertySearchRouter.get("/", async (req, res) => {
   /* Pagination */
   let page = req.query.page || 1; // Current page number
   const limit = req.query.size || 10; // Number of items per page
-  if(page==0){
-    page=1;
+  if (page == 0) {
+    page = 1;
   }
   let skip = (page - 1) * limit; // Calculate the number of items to skip
- 
 
+  const { search, status } = req.query;
   /* Search */
-  const { search } = req.query;
-  console.log("Searching...", search);
+  console.log("Search::", req.query);
 
   /* Filter */
-  const filter = {
-    $or: [
+  // Create an empty filter object
+  const filter = {};
+
+  // Apply search filter if search query parameter is provided
+  if (search) {
+    filter.$or = [
       { address: { $regex: new RegExp(search, "i") } },
       { city: { $regex: new RegExp(search, "i") } },
       { state: { $regex: new RegExp(search, "i") } },
-    ],
-  };
+    ];
+  }
+
+  // Apply status filter if status query parameter is provided
+  if (status) {
+    if (status.toLowerCase() === "draft") {
+      filter.$or = [
+        { status: { $regex: new RegExp(status, "i") } },
+        { status: { $exists: false } },
+      ];
+    } else {
+      filter.status = { $regex: new RegExp(status, "i") };
+    }
+  }
 
   /* Additional Filters*/
   const homeType = req.query.homeType;
-  const minPrice =  req.query.minPrice;
+  const minPrice = req.query.minPrice;
   const maxPrice = req.query.maxPrice;
   if (homeType) {
     filter.homeType = homeType;
@@ -44,29 +59,30 @@ propertySearchRouter.get("/", async (req, res) => {
       filter.price.$lte = maxPrice;
     }
   }
-/* Sorting */
-const sortField =  req.query.sort || "homeType"; // Default sort field is 'price'
-let sort = sortField.toUpperCase();
-let sortOrder = "asc"; // Default sort order is ascending
+  /* Sorting */
+  const sortField = req.query.sort || "homeType"; // Default sort field is 'price'
+  let sort = sortField.toUpperCase();
+  let sortOrder = "asc"; // Default sort order is ascending
 
-if (sort == "High to Low".toUpperCase()) {
-  sort = "price";
-  sortOrder = "desc";
-} else if (sort == "Low to High".toUpperCase()) {
-  sort = "price";
-} else if (sort == 'Popular'.toUpperCase()){
-  sort = "sqFt";
-} else {
-  sort = "homeType";
-}
+  if (sort == "High to Low".toUpperCase()) {
+    sort = "price";
+    sortOrder = "desc";
+  } else if (sort == "Low to High".toUpperCase()) {
+    sort = "price";
+  } else if (sort == "Popular".toUpperCase()) {
+    sort = "sqFt";
+  } else {
+    sort = "homeType";
+  }
 
-const sortOptions = {};
-sortOptions[sort] = sortOrder === "desc" ? -1 : 1;
+  const sortOptions = {};
+  sortOptions[sort] = sortOrder === "desc" ? -1 : 1;
 
-
+  /* Headers */
+  const propertyFields = Object.keys(propertyDB.schema.obj);
+  console.log(propertyFields);
 
   try {
-
     // Perform a case-insensitive search using regular expressions, apply the filters, and sort the results
     const query = propertyDB.find(filter);
     const totalRecords = await query.countDocuments(); // Total number of records matching the query
@@ -82,7 +98,6 @@ sortOptions[sort] = sortOrder === "desc" ? -1 : 1;
       .limit(limit)
       .exec();
 
-
     // Send the results as the response
     res.status(200).json({
       message: "property data fetched successfully",
@@ -92,12 +107,11 @@ sortOptions[sort] = sortOrder === "desc" ? -1 : 1;
       totalRecords: totalRecords,
       numberofpages: totalPages,
       property: results,
+      headers: propertyFields,
     });
   } catch (error) {
     console.error("Error performing property search:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while performing property search" });
+    res.status(500).json({ error: "An error occurred while performing property search" });
   }
 });
 
--- src/service/propertyService.js
+++ src/service/propertyService.js
@@ -1,122 +1,96 @@
 const propertyRouter = require("express").Router(),
   { propertyDB } = require("../model/property");
-const {sendEmail} = require("../config/email");
+const { sendEmail } = require("../config/email");
 const propertySearchRouter = require("./propertySearchService");
 
 /**
  * Creates a new property.
  */
 propertyRouter.post("", async (req, res, next) => {
-  const property = new propertyDB({
-    id: req.body.id,
-    homeType: req.body.homeType,
-    isBuy: req.body.isBuy,
-    bed: req.body.bed,
-    bath: req.body.bath,
-    price: req.body.price,
-    currency: req.body.currency,
-    sqFt: req.body.sqFt,
-    address: req.body.address,
-    city: req.body.city,
-    state: req.body.state,
-    title: req.body.title,
-    description: req.body.description,
-    images: req.body.images,
-    img1: req.body.img1,
-    img2: req.body.img2,
-    yearBuilt: req.body.yearBuilt,
-    contactName: req.body.name,
-    contactEmail: req.body.email,
-    contactPhone: req.body.phone,
-    sellerType: req.body.sellerType,
-    isListed: req.body.isListed,
-    sellDuration: req.body.duration,
-    amenities: req.body.amenities,
-    status: req.body.status || "DRAFT",
-  });
- 
-  property.save();
-  //console.log(property);
+  try {
+    const property = new propertyDB({
+      ...req.body,
+      contactName: req.body.name,
+      contactEmail: req.body.email,
+      contactPhone: req.body.phone,
 
-  await sendEmail(property);
+      sellDuration: req.body.duration,
+      status: req.body.status || "DRAFT",
+    });
 
-  res.status(201).json({
-    message: "property data added successfully",
-  });
+    property.save();
+
+    await sendEmail(property);
+
+    res.status(201).json({
+      message: "property data added successfully",
+      property: property
+    });
+  } catch (error) {
+    console.error("Error occurred while creating a property", error);
+    res.status(500).json({ error: "An error occurred while creating a property" });
+  }
 });
 
-/**
- * Returns list of propertys
- */
-propertyRouter.get("", async (req, res, next) => {
-  const { search } = req.query;
-  console.log("Search::", req.query);
-  // console.log("Searching in property...", search, search == null, search != null, search == undefined, search == '', search.length);
-  if (search) {
-    propertySearchRouter(req, res);
-  } else {
-    console.log("Fetching...");
-    propertyDB.find().then((docs) => {
-      res.status(200).json({
-        message: "Data Fetched Successfully",
-        property: docs,
-      });
+propertyRouter.put("/:id", async (req, res) => {
+  try {
+    const propertyId = req.params.id;
+    const updates = req.body;
+
+    const updatedProperty = await propertyDB.findByIdAndUpdate(
+      propertyId,
+      updates,
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Property updated successfully",
+      property: updatedProperty,
     });
+  } catch (error) {
+    console.error("Error updating property:", error);
+    res.status(500).json({ error: "An error occurred while updating the property" });
   }
+});
+
+
+const processQueryParams = (req, res, next) => {
+  // Process and validate query parameters here
+  const { status } = req.query;
+
+  //  Ensure 'status' parameter is valid
+  const validStatuses = ["approved", "draft", "rejected"];
+  if (status && !validStatuses.includes(status.toLowerCase())) {
+    return res.status(400).json({ error: "Invalid status parameter. Possible Values are: " + validStatuses });
+  }
+
+  // Continue to the next middleware or route handler
+  next();
+};
+
+/**
+ * Returns list of properties
+ */
+propertyRouter.get("", processQueryParams, async (req, res, next) => {
+    /* Forwarded to Search and Filter Service. */
+    propertySearchRouter(req, res);
+  
 });
 
 /**
  * Returns a property by id.
  */
-propertyRouter.get("/:id", (req, res, next) => {
-  propertyDB.findById(req.params.id).then((propertydb) => {
+propertyRouter.get("/:id", async (req, res, next) => {
+  try {
+    const response = await propertyDB.findById(req.params.id);
     res.status(200).json({
       message: "Id fetched successfully!",
-      property: req.params.id,
-      id: propertydb.id,
-      homeType: propertydb.homeType,
-      isBuy: propertydb.isBuy,
-      bed: propertydb.bed,
-      bath: propertydb.bath,
-      price: propertydb.price,
-      currency: propertydb.currency,
-      sqFt: propertydb.sqFt,
-      address: propertydb.address,
-      city: propertydb.city,
-      state: propertydb.state,
-      title: propertydb.title,
-      description: propertydb.description,
-      images: propertydb.iamges,
-      img1: propertydb.img1,
-      yearBuilt: propertydb.yearBuilt,
-      contactName: propertydb.contactName,
-      contactEmail: propertydb.contactEmail,
-      contactPhone: propertydb.contactPhone,
-      sellerType: propertydb.sellerType,
-      isListed: propertydb.isListed,
-      sellDuration: propertydb.sellDuration,
-      amenities: propertydb.amenities,
-      status: propertydb.status,
-    });
-  });
-});
-
-propertyRouter.put('/:id', async (req, res) => {
-  try {
-    const propertyId = req.params.id;
-    const updates = req.body;
-
-    const updatedProperty = await propertyDB.findByIdAndUpdate(propertyId, updates, { new: true });
-
-    res.status(200).json({
-      message: 'Property updated successfully',
-      property: updatedProperty
+      property: response,
     });
   } catch (error) {
-    console.error('Error updating property:', error);
-    res.status(500).json({ error: 'An error occurred while updating the property' });
+    console.error("Error fetching property by id:", error);
+    res.status(500).json({ error: "An error occurred while fetching the property by id" });
   }
 });
-
 
 module.exports = propertyRouter;
