9b3171cd6434a269e98bdda36a83cf7f891775a5
Pooja4183 <poojatomar.jt@gmail.com>
2023-07-06 23:03:59 +0530

    changed again

--- src/config/app-mongo.js
+++ src/config/app-mongo.js
@@ -3,8 +3,9 @@ const connect = () => {
   mongoose
     .connect(
      // 'mongodb+srv://sellanyhome55:arush555@sellanyhomecluster.cmadjgc.mongodb.net/test?retryWrites=true&w=majority'
-     'mongodb+srv://superuser:user123@localhost:27017/admin?retryWrites=true&w=majority'
-      ,
+     //'mongodb+srv://superuser:user123@localhost:27017/admin?retryWrites=true&w=majority'
+     ' mongodb://superuser:user123@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1' 
+     ,
      { 
       useNewUrlParser: true,
       useUnifiedTopology: true
