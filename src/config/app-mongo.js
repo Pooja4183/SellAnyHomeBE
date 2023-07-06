d8d67ccf9d052903c61b1af50342740c7d785ff4
Pooja4183 <poojatomar.jt@gmail.com>
2023-07-06 22:53:24 +0530

    changed mongodb connection url

--- src/config/app-mongo.js
+++ src/config/app-mongo.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const connect = () => {
   mongoose
     .connect(
-      'mongodb+srv://sellanyhome55:arush555@sellanyhomecluster.cmadjgc.mongodb.net/test?retryWrites=true&w=majority'
-     ,
+     // 'mongodb+srv://sellanyhome55:arush555@sellanyhomecluster.cmadjgc.mongodb.net/test?retryWrites=true&w=majority'
+     'mongodb://superuser:user123@localhost:27017/admin'
+      ,
      { 
       useNewUrlParser: true,
       useUnifiedTopology: true
